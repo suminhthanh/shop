7e0c9275f26ed268c8563b05a465ad850b389785
suminhthanh <smthanh@gmail.com>
2018-11-14 16:34:48 +0700

    - add func in CategoryTest

--- src/main/java/net/vnict/shop/config/WebSecurityConfig.java
+++ src/main/java/net/vnict/shop/config/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -34,24 +33,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
-	
-	
-    // Authentication and Authorization
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        	.authorizeRequests()
-    			.antMatchers("admin/**").access("hasRole('ADMIN')")
-    			.and()
-    		.formLogin()
-            	.loginPage("/login")
-            	.usernameParameter("email")
-            	.passwordParameter("password")
-            	.defaultSuccessUrl("/")
-            	.failureUrl("/login?error")
-            	.and()
-       		.csrf()
-       			.disable();
-    }
+
 }
--- src/test/java/net/vnict/shop/CategoryTest.java
+++ src/test/java/net/vnict/shop/CategoryTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @RunWith(SpringRunner.class)
@@ -21,19 +23,33 @@ public class CategoryTest {
     CategoryService categoryService;
 
     @Test
-    public void testGetCategory() {
+    public void testCategory_getCategoryOK() {
         Category category = categoryService.findOne(1);
         assertThat("Pizza").isEqualTo(category.getName());
     }
 
     @Test
-    public void testSaveCategory() {
+    public void testCategory_getCategoryNotFound() {
+        Category category = categoryService.findOne(22222);
+        assertThat("Pizza").isEqualTo(category.getName());
+    }
+
+    @Test
+    public void testCategory_saveCategoryOK() {
         Category category = new Category();
         category.setName("Drink");
         categoryService.save(category);
         List<Category> categories = (List<Category>) categoryService.findAll();
        Category category1 = categoryService.findOne(categories.size()-1);
        assertThat(category.getName()).isEqualTo(category1.getName());
+    }
+
+    @Test
+    public void testCategory_saveCategoryFail() {
+        Category category = null;
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> {
+            categoryService.save(category);
+        });
     }
 
 }
